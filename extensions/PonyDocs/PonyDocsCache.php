e3d4e0627c402069a872c0c471b0e84f6c32e9d4
Russell Uman <ruman@splunk.com>
2014-04-01 11:58:05 -0700

    start over from scratch for WEB-8848, and this time use replace +review WEBCR-189

--- extensions/PonyDocs/PonyDocsCache.php
+++ extensions/PonyDocs/PonyDocsCache.php
@@ -9,7 +9,6 @@ class PonyDocsCache
 	private function __construct()
 	{
 		$this->dbr = wfGetDB(DB_MASTER);
-		$this->expire();
 	}
 	
 	static public function & getInstance() 
@@ -28,7 +27,7 @@ class PonyDocsCache
 				$expires = time() + 3600;
 			}
 			$data = mysql_real_escape_string(serialize($data));
-			$query = "INSERT IGNORE INTO ponydocs_cache VALUES('$key', '$expires',  '$data')";
+			$query = "REPLACE INTO ponydocs_cache VALUES('$key', '$expires',  '$data')";
 			try {
 				$this->dbr->query($query);
 			} catch (Exception $ex){
@@ -41,7 +40,7 @@ class PonyDocsCache
 	public function get( $key )
 	{
 		if(PONYDOCS_CACHE_ENABLED) {
-			$query = "SELECT *  FROM ponydocs_cache WHERE cachekey = '$key'";
+			$query = "SELECT *  FROM ponydocs_cache WHERE cachekey = '$key' AND expires > UNIX_TIMESTAMP()";
 			try {
 				$res = $this->dbr->query($query);
 				$obj = $this->dbr->fetchObject($res);
@@ -67,17 +66,4 @@ class PonyDocsCache
 		}
 		return true;
 	}	
-
-	public function expire() {
-		if(PONYDOCS_CACHE_ENABLED) {
-			$now = time();
-			$query = "DELETE FROM ponydocs_cache WHERE expires < $now";
-			try {
-				$res = $this->dbr->query($query);
-			} catch(Exception $ex) {
-				error_log("FATAL [PonyDocsCache::expire] DB call failed on Line " . $ex->getLine()." on file " . $ex->getFile(). ", error Message is: \n" . $ex->getMessage(). " Stack Trace Is: " . $ex->getTraceAsString());
-			}
-		}
-		return true;
-	}
 };
