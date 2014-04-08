b5dad8c377118af9c936501dadff73db8ada3a55
Russell Uman <ruman@splunk.com>
2014-04-08 15:15:28 -0700

    documentation clean-up and remove commented-out code for WEB-8147 +review WEBCR-205

--- extensions/PonyDocs/PonyDocsTopic.php
+++ extensions/PonyDocs/PonyDocsTopic.php
@@ -3,9 +3,9 @@ if ( !defined( 'MEDIAWIKI' ))
 	die( "PonyDocs MediaWiki Extension" );
 
 /**
- * Provides special handling methods/functionality for a "topic", which is basically an article being viewed or
- * updated in some manner.  It is passed the article object and provides methods for retrieving data, processing
- * and static methods for general tool relation functions.
+ * Provides special handling methods/functionality for a "topic", which is an article being viewed or updated in some manner.
+ * It is passed the article object and provides methods for retrieving data, 
+ * processing and static methods for general tool relation functions.
  */
 
 class PonyDocsTopic {
@@ -84,57 +84,9 @@ class PonyDocsTopic {
 	}
 
 	/**
-	 * Return an array of versions for the supplied article.  These are saved as category tags so we simply need to find the page_id from
-	 * the article and find anything in the categorylinks table.  Return a list with each element being a PonyDocsVersion object.
-	 * 
-	 * @param boolean $reload If true, force reload from database; else used cache copy (if found).
-	 * @return array
-	 * @deprecated use getProductVersions
-	 */
-/*	public function getVersions( $reload = false )
-	{
-		if( sizeof( $this->versions ) && !$reload )
-			return $this->versions;
-
-		$dbr = wfGetDB( DB_SLAVE );
-		$revision = $this->pArticle->mRevision;
-		
-		//$res = $dbr->select( 'categorylinks', 'cl_to', "cl_from = '" . $revision->mPage . "'", __METHOD__ );
-		$res = $dbr->select( 'categorylinks', 'cl_to',
-							"cl_sortkey = '" . $dbr->strencode(  $this->pTitle->__toString( )) . "'", __METHOD__ );		
-
-
-		$tempVersions = array();
-
-		while ( $row = $dbr->fetchObject( $res ))
-		{
-			if( preg_match( '/^v:(.*)/i', $row->cl_to, $match ))
-			{
-				$v = PonyDocsVersion::GetVersionByName( $match[1] );
-				if( $v )
-					$tempVersions[] = $v;
-			}
-		}
-
-		// Sort by Version, by doing a natural sort
-		// Also remove any duplicates.
-		$sortArray = array();
-		foreach($tempVersions as $index => $version) {
-			if(!in_array($version->getName(), $sortArray)) {
-				$sortArray[(string)$index] = $version->getName();
-			}
-		}
-		natsort($sortArray);
-		foreach($sortArray as $targetIndex => $verName) {
-			$this->versions[] = $tempVersions[$targetIndex];
-		}
-		
-		return $this->versions;
-	}
-*/
-	/**
-	 * Return an array of versions for the supplied article.  These are saved as category tags so we simply need to find the page_id from
-	 * the article and find anything in the categorylinks table.  Return a list with each element being a PonyDocsVersion object.
+	 * Return an array of versions for the supplied article.
+	 * These are saved as category tags so we need to find the page_id from the article and anything in the categorylinks table.
+	 * Return a list with each element being a PonyDocsVersion object.
 	 * 
 	 * @param boolean $reload If true, force reload from database; else used cache copy (if found).
 	 * @return array
@@ -161,8 +113,7 @@ class PonyDocsTopic {
 				}
 			}
 		}
-		// Sort by Version, by doing a natural sort
-		// Also remove any duplicates.
+		// Sort by Version, by doing a natural sort. Also remove any duplicates.
 		/// FIXME - what is this really doing? tempVersions index is int per above code!
 		$sortArray = array();
 		foreach ( $tempVersions as $index => $version ) {
@@ -179,8 +130,8 @@ class PonyDocsTopic {
 	}
 
 	/**
-	 * Given a 'base' topic name (Documentation:User:HowToFoo), find the proper topic name based on selected version.  So if 2.1 is selected and we have
-	 * a HowToFoo2.0 tagged for 2.1, return HowToFoo2.0.
+	 * Given a 'base' topic name (Documentation:User:HowToFoo), find the proper topic name based on selected version.
+	 * If 2.1 is selected and we have a HowToFoo2.0 tagged for 2.1, return HowToFoo2.0.
 	 *
 	 * @static
 	 * @param string $baseTopic
@@ -205,7 +156,8 @@ class PonyDocsTopic {
 	/**
 	 * CHANGE THIS TO CACHE THESE IN MEMCACHE OR WHATEVER.
 	 *
-	 * This takes a title (text form) and extracts the H1 content for the title and returns it.  This is used in display (heading and TOC).
+	 * This takes a title (text form) and extracts the H1 content for the title and returns it.
+	 * This is used in display (heading and TOC).
 	 * 
 	 * @static
 	 * @param string $title The text form of the title to get the H1 content for.
@@ -267,11 +219,12 @@ class PonyDocsTopic {
 	}
 
 	/**
-	 * Not currently used, but wanted to save the code in case we need it.  This takes an article content and extracts all [[]] style
-	 * links from it into a match array.  Each array in the list contains 3 indices:
-	 * 	0= Complete matching [[ ]] wiki link tag including braces.
-	 *  1= The actual link name (such as Documentation:User:HowToFoo3.0).
-	 * 	2= Any alternate text should it be provided;  blank otherwise (but still defined).
+	 * Not currently used, but wanted to save the code in case we need it.
+	 * This takes an article content and extracts all [[]] style links from it into a match array.
+	 * Each array in the list contains 3 indices:
+	 * - 0 = Complete matching [[ ]] wiki link tag including braces.
+	 * - 1 = The actual link name (such as Documentation:User:HowToFoo3.0).
+	 * - 2 = Any alternate text should it be provided;  blank otherwise (but still defined).
 	 * 
 	 * The regular expression was a massive headache.
 	 * 
@@ -286,47 +239,22 @@ class PonyDocsTopic {
 	}
 
 	/**
-	 * Another non-used function which we may need if we have to parse out the section headers of an article (which is very likely).  This
 	 * parses out all the headers in the form:
 	 * 	= Header =
-	 * It requires it be valid MediaWiki, so it must have the same number of '=' on each side.  One set is H1, two is H2, and so forth.  The
+	 * It requires valid MediaWiki markup, so it must have the same number of '=' on each side.
+	 * One set is H1, two is H2, and so forth.  The
 	 * results array has:
-	 * 	0= Complete match with equal signs.
-	 *  1= The header text inside the equal signs.
-	 *  2= This will contain the left hand side set of equal signs, so strlen() this to get the header level.
-	 *
-	 * @return array
-	 */
-/*	public function parseSections( )
-	{
-		$content = preg_replace( '/\<nowiki\>(.*)\<\/nowiki\>/i', '', $this->pArticle->mContent );
-
-		$re = "/(=+)([\() A-Za-z0-9?._+%$#@!&*~`'\\\"{}|,.<>-]+)(\\1)/";
-		if( preg_match_all( $re, $content, $matches, PREG_SET_ORDER ))
-			return $matches;
-		return array( );
-	}
-*/
-	/**
-	 * parses out all the headers in the form:
-	 * 	= Header =
-	 * It requires it be valid MediaWiki, so it must have the same number of '=' on each side.  One set is H1, two is H2, and so forth.  The
-	 * results array has:
-	 * 	0= Complete match with equal signs.
-	 *  1= The header text inside the equal signs.
-	 *  2= This will contain the left hand side set of equal signs, so strlen() this to get the header level.
+	 * - 0 = Complete match with equal signs.
+	 * - 1 = The header text inside the equal signs.
+	 * - 2 = This will contain the left hand side set of equal signs, so strlen() this to get the header level.
 	 *
 	 * @return array
 	 */
 	public function parseSections()	{
-
-		//$content = preg_replace( '/\<nowiki\>(.*)\<\/nowiki\>/i', '', $this->pArticle->mContent );
 		$content = str_replace("<nowiki>", "", $this->pArticle->mContent);
 		$content = str_replace("</nowiki>", "", $content);
 
-		//$re = "/(=+)([\() A-Za-z0-9?._+%$#@!/&*~`'\\\"{}|,.<>-]+)(\\1)/";
-		// We don't need such a long regex.  Simply encapsulating everything in 
-		// header element.
+		// We don't need such a long regex.  Simply encapsulating everything in header element.
 		$re = "/(=+)([^=]*)(=+)\n/";
 		if ( preg_match_all( $re, $content, $matches, PREG_SET_ORDER ) ) {
 			foreach ( $matches as &$match ) {
@@ -395,7 +323,8 @@ class PonyDocsTopic {
 	}
 
 	/**
-	 * Get 'base' topic name, meaning with version stripped off.  Only works if a Documentation NS topic, else returns empty string.
+	 * Get 'base' topic name, meaning with version stripped off.
+	 * Only works if a Documentation NS topic, else returns empty string.
 	 *
 	 * @return string
 	 */
@@ -407,7 +336,3 @@ class PonyDocsTopic {
 		return '';
 	}
 };
-
-/**
- * End of file.
- */
