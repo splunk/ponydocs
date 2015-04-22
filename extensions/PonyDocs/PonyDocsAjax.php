c1252a025cf0336ab122ef6934c53cb3c710d4b0
salim-mujawar <smujawar@splunk.com>
2015-04-23 00:57:06 +0530

    did changes as per WEBCR for WEB-10264 +review WEBCR-901

--- extensions/PonyDocs/PonyDocsAjax.php
+++ extensions/PonyDocs/PonyDocsAjax.php
@@ -166,12 +166,12 @@ function efPonyDocsAjaxChangeVersion( $product, $version, $title, $force = false
 			error_log("DEBUG [" . __METHOD__ . ":" . __LINE__ . "] ajax redirect rule 4");
 		}
 	} elseif ( preg_match( '/' . PONYDOCS_DOCUMENTATION_NAMESPACE_NAME . '\/(.*)\/(.*)\/(.*)/i', $title, $match )) {
-                //Redirection for WEB-10264
-                $response->addText( str_replace( '$1', PONYDOCS_DOCUMENTATION_NAMESPACE_NAME . '/' . $product . '/' . $version . '/' . $match[3] , $wgArticlePath ));
-                if (PONYDOCS_REDIRECT_DEBUG) {
-                        error_log("DEBUG [" . __METHOD__ . ":" . __LINE__ . "] ajax redirect rule to switch versions on a static manual");
-                }
-        } else {
+		//Redirection for WEB-10264
+		$response->addText( str_replace( '$1', PONYDOCS_DOCUMENTATION_NAMESPACE_NAME . '/' . $product . '/' . $version . '/' . $match[3] , $wgArticlePath ));
+		if (PONYDOCS_REDIRECT_DEBUG) {
+			error_log("DEBUG [" . __METHOD__ . ":" . __LINE__ . "] ajax redirect rule to switch versions on a static manual");
+		}
+	} else {
 		$add_text = str_replace( '$1', $defaultTitle . '/' . $product . '/' . $version, $wgArticlePath );
 		$response->addText( $add_text );
 		if (PONYDOCS_REDIRECT_DEBUG) {error_log("DEBUG [" . __METHOD__ . ":" . __LINE__ . "] ajax redirect rule 5");}
