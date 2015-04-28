b43ae0ce4c9e452b9eaa09a6c9745180db99fa68
salim-mujawar <smujawar@splunk.com>
2015-04-28 17:42:27 +0530

    If you hit the URL for Product-Version-Manual that has no TOC you get a white screen for WEB-10038 +review WEBCR

--- extensions/PonyDocs/PonyDocsTOC.php
+++ extensions/PonyDocs/PonyDocsTOC.php
@@ -353,6 +353,13 @@ class PonyDocsTOC
 
 		$currentIndex = -1;
 		$start = array();
+
+		// Redirect to /Documentation for WEB-10038
+		if ( is_array ($toc) && sizeof($toc) == 0 ) {
+			$defaultRedirect = PonyDocsExtension::getDefaultUrl();
+			header('Location: ' . $defaultRedirect);
+		}
+		
 		// Go through and determine start, prev, next and current elements.
 
 		foreach ( $toc as $idx => &$entry ) {
